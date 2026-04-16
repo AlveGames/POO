b6cb18e77efd18cf8437eedbcf9134ec1c3efee9
alvegames <renjiYT1978@gmail.com>
2026-04-15 19:45:42 -0500

    se agrega fase 2 para futuro codigo

--- ejercicio1.py
+++
@@ -1,5 +0,0 @@
-def saludando_gente():
-    print("klk mi gente")
-    print("aqui andamo comiendono un mangito classic")
-
-saludando_gente()
---
+++ fase1.py
@@ -0,0 +1,15 @@
+#ejercicio 1
+# funciones sin parametros y sin retorno
+def saludando_gente():
+    print("klk mi gente")
+    print("aqui andamo comiendono un mangito classic")
+
+saludando_gente()
+
+# --------------------------------------------------------------------
+#lo unico que hace es hablar cuando se le llama a una funcion
+# --------------------------------------------------------------------
+
+#ejercicio 2
+#Funciones con parámetros y retorno
+
