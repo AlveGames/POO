478ede8ec32ad2f824fc5649bbbea671de66b1e0
alvegames <renjiYT1978@gmail.com>
2026-04-21 14:18:55 -0500

    comienzo de herencias

---
+++ herencias.py
@@ -0,0 +1,12 @@
+# class animal():
+#     def __init__(self,raza, especie):
+#         self.raza = raza
+#         self.especie = especie
+
+#     def sonido(self):
+#         print("rawrr....")
+
+# class cuadrupedo(animal):
+#     pass
+
+# animal1 = animal("Leopardo de Arabia", "Panthera pardus nimr")
